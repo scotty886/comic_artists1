7a0ca2a5b7ba189e55513d218594c0f977ff0d8e
Terence Scott <scott.terence7@gmail.com>
2025-03-16 16:50:30 -0400

    initial commit

---
+++ comicshop/urls.py
@@ -0,0 +1,22 @@
+
+from django.urls import path
+
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('details/<int:pk>/', views.details, name='details'),
+    path('product_entry/', views.product_entry, name='product_entry'),
+    path('categories/<str:foo>', views.categories, name='categories'),
+    path('search/', views.search, name='search'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('update_user/', views.update_user, name='update_user'),
+    path('update_password/', views.update_password, name='update_password'),
+    path('update_profile/', views.update_profile, name='update_profile'),
+    path('users_list/', views.users_list, name='users_list'),
+    path('add_product/', views.add_product, name='add_product'),
+]
