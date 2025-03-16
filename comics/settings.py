01bed92428ac3ac37bfcac036c306e90f0da61f6
Terence Scott <scott.terence7@gmail.com>
2025-03-16 18:48:41 -0400

    changed csrf trusted origins

--- comics/settings.py
+++ comics/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-l=46o2!qvj!h(ae-dxpvctbnvj*6^nojsy@k_v068z6i7=651c
 DEBUG = True
 
 ALLOWED_HOSTS = ["comicartists1-production.up.railway.app", "https://comicartists1-production.up.railway.app/"]
-CSRF_TRUSTED_ORIGINS = ["comicartists1-production.up.railway.app", "https://comicartists1-production.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = [ "https://comicartists1-production.up.railway.app/"]
 
 
 # Application definition
